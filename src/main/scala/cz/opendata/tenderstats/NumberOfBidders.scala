5dfe4a91e4a1ed0c286d20457dc24196ce7830e5
propi <prozeman@gmail.com>
2014-09-09 17:27:36 +0200

    Number of bidders prediction fixes

--- src/main/scala/cz/opendata/tenderstats/NumberOfBidders.scala
+++ src/main/scala/cz/opendata/tenderstats/NumberOfBidders.scala
@@ -3,6 +3,7 @@ package cz.opendata.tenderstats
 import com.github.jsonldjava.core.JsonLdOptions
 import com.github.jsonldjava.core.JsonLdProcessor
 import com.github.jsonldjava.utils.JsonUtils
+import com.hp.hpl.jena.rdf.model.ResourceFactory
 import cz.opendata.tenderstats.utils.Lift
 import cz.opendata.tenderstats.utils.NonEmptyString
 import java.io.ByteArrayOutputStream
@@ -47,11 +48,12 @@ object NumberOfBidders {
     val baos = new ByteArrayOutputStream
     debug(s"Sending POST to: $endpoint with entity $uri from graph $graph")
     try {
-      Sparql
+      val m = Sparql
         .privateQuery(Sparql.template("resource_description_extended.mustache", Map("source-graph" -> graph, "resource" -> uri)))
         .execConstruct
-        .write(baos, "JSONLD")
-      val x = "\"" + JsonUtils.toString(JsonLdProcessor.frame(JsonUtils.fromString(baos.toString("UTF-8")), jsonLdFrame, jsonLdOptions)).replaceAll("\"", """ \\" """.trim) + "\""
+      m.getResource(uri).addProperty(ResourceFactory.createProperty(Config.cc.getPrefix("pc"), "kind"), "")
+      m.write(baos, "JSONLD")
+      val x = JsonUtils.toString(JsonLdProcessor.frame(JsonUtils.fromString(baos.toString("UTF-8")), jsonLdFrame, jsonLdOptions))
       debug(s"POST body content is:\n$x")
       val rs = client.target(endpoint).request.post(Entity.entity(x, "application/ld+json"), classOf[String])
       debug(s"POST response is:\n$rs")
